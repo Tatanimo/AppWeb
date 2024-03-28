1639b8b3e03a7f75c6c102d97c525e3797db9ccc
LucasBertaud <l.bertaud@hotmail.com>
2024-03-28 18:16:57 +0100

    fix (init:db): symfony -> php bin/consoleà

--- src/Command/InitializeDatabaseCommand.php
+++ src/Command/InitializeDatabaseCommand.php
@@ -4,14 +4,10 @@ namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Helper\ProgressIndicator;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 
@@ -89,8 +85,8 @@ class InitializeDatabaseCommand extends Command
         // Process D:L:F 
         $io->info('Procédure de génération des fixtures');
         $process = new Process([
-            'symfony',
-            'console',
+            'php',
+            'bin/console',
             'd:f:l',
             '--append'
         ]);
