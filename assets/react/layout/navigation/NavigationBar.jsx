cc3a225ecf6174a29ad71c51dc7b94ca3cde4dcf
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-04-01 16:59:10 +0200

    react component

---
+++ assets/react/layout/navigation/NavigationBar.jsx
@@ -0,0 +1,23 @@
+export default function NavigationBar() {
+    return (
+        <nav>
+            <div className="w-64 h-14 border">
+                Image
+            </div>
+            <ul>
+                <li>
+                    <a href="https://click-n-party.mds-lille.yt/">Lien 1</a>
+                </li>
+                <li>
+                    <a href="https://click-n-party.mds-lille.yt/">Lien 1</a>
+                </li>
+                <li>
+                    <a href="https://click-n-party.mds-lille.yt/">Lien 1</a>
+                </li>
+                <li>
+                    <a href="https://click-n-party.mds-lille.yt/">Lien 1</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
