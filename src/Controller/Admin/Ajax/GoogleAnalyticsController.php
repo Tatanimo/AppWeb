2ee4e29335af2f35b2a7ba0996c2cbb7fae6db0a
LucasBertaud <l.bertaud@hotmail.com>
2024-04-03 10:03:26 +0200

    feat(google analytics): fetchActiveUsers

--- src/Controller/Admin/Ajax/GoogleAnalyticsController.php
+++ src/Controller/Admin/Ajax/GoogleAnalyticsController.php
@@ -6,45 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Google\Analytics\Data\V1beta\Client\BetaAnalyticsDataClient;
-use Google\Analytics\Data\V1beta\DateRange;
-use Google\Analytics\Data\V1beta\Dimension;
+use Google\Analytics\Data\V1beta\RunRealtimeReportRequest;
 use Google\Analytics\Data\V1beta\Metric;
-use Google\Analytics\Data\V1beta\RunReportRequest;
 
 class GoogleAnalyticsController extends AbstractController
 {
     #[Route('/admin/ajax/google/analytics', name: 'app_admin_ajax_google_analytics')]
-    public function fetch(): JsonResponse
+    public function fetchActiveUsers(): JsonResponse
     {
-        putenv("GOOGLE_APPLICATION_CREDENTIALS=" . $this->getParameter('kernel.project_dir').'google_credentials.json');
+        putenv("GOOGLE_APPLICATION_CREDENTIALS=" . $this->getParameter('kernel.project_dir').'/google_credentials.json');
         $property_id = $_ENV['GOOGLE_PROPERTY_ID'];
 
         $client = new BetaAnalyticsDataClient();
 
-        $request = (new RunReportRequest())
+        $request = (new RunRealtimeReportRequest())
             ->setProperty('properties/' . $property_id)
-            ->setDateRanges([
-                new DateRange([
-                    'start_date' => '2020-03-31',
-                    'end_date' => 'today',
-                ]),
-            ])
-            ->setDimensions([new Dimension([
-                    'name' => 'city',
-                ]),
-            ])
             ->setMetrics([new Metric([
                     'name' => 'activeUsers',
                 ])
             ]);
-        $response = $client->runReport($request);
+        $response = $client->runRealtimeReport($request);
 
-        // Print results of an API call.
-        print 'Report result: ' . PHP_EOL;
-
+        $var = null;
         foreach ($response->getRows() as $row) {
-            $var = $row->getDimensionValues()[0]->getValue()
-                . ' ' . $row->getMetricValues()[0]->getValue() . PHP_EOL;
+            $var = $row->getMetricValues()[0]->getValue();
         }
         return $this->json($var);
     }
