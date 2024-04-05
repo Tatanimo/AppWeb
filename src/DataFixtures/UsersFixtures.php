d3c7847349e584a9def8cb6b05c711dae0096351
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-04-05 11:46:41 +0200

    feat: setCreatedDate UsersFixtures.php

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -31,7 +31,8 @@ class UsersFixtures extends Fixture
             $hashedPassword = $this->hasher->hashPassword($user, $password);
 
             $user->setAddress($faker->address())->setBirthdate($faker->datetime())->setCities($cities)->setEmail($faker->email())->setFirstName($faker->firstName())->setLastName($faker->lastName())->setPhoneNumber($faker->e164PhoneNumber())->setRoles(['ROLE_USER'])->setPassword($hashedPassword)->setIban($faker->iban('FR'));
-
+            $user->setCreatedDate($faker->dateTimeThisDecade());
+            
             $manager->persist($user);
         }
 
@@ -46,6 +47,7 @@ class UsersFixtures extends Fixture
             $hashedPassword = $this->hasher->hashPassword($user, $password);
 
             $user->setAddress($faker->address())->setBirthdate($faker->datetime())->setCities($cities)->setEmail($faker->email())->setFirstName($faker->firstName())->setLastName($faker->lastName())->setPhoneNumber($faker->e164PhoneNumber())->setRoles(['ROLE_ADMIN'])->setPassword($hashedPassword)->setIban($faker->iban('FR'));
+            $user->setCreatedDate($faker->dateTimeThisDecade());
 
             $manager->persist($user);
         }
@@ -59,6 +61,7 @@ class UsersFixtures extends Fixture
         $hashedPassword = $this->hasher->hashPassword($user, $password);
 
         $user->setAddress($faker->address())->setBirthdate($faker->datetime())->setCities($cities)->setEmail("admin@admin.fr")->setFirstName($faker->firstName())->setLastName($faker->lastName())->setPhoneNumber($faker->e164PhoneNumber())->setRoles(['ROLE_ADMIN'])->setPassword($hashedPassword)->setIban($faker->iban('FR'));
+        $user->setCreatedDate($faker->dateTimeThisDecade());
 
         $manager->persist($user);
 
