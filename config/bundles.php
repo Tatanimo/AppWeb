cf6fbc1d8e03367f260288f9a223ec32169e1d3f
LucasBertaud <l.bertaud@hotmail.com>
2024-04-20 20:42:29 +0200

    build: remove lexik/jwt

--- config/bundles.php
+++ config/bundles.php
@@ -17,6 +17,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
