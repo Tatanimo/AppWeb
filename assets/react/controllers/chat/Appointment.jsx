f14ea6de22ae6d7dd5dd33a6716102cfa048381f
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-05-22 23:12:58 +0200

    feat: adding component for appointment

---
+++ assets/react/controllers/chat/Appointment.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import axios from "axios";
+
+export default function Appointment(contentState, authorId, userId, room, id) {
+    const appointment = JSON.parse(contentState.contentState);
+
+    const answeredAppointment = async (appointment, answer) => {
+        await axios.post("/ajax/appointments", appointment, {
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+            },
+        })
+            .then(res => {
+                appointment.answered = true;
+                appointment.accepted = answer;
+                updateMessage(appointment).then(
+                    setContentState(JSON.stringify(appointment)),
+                );
+            })
+            .catch(err => console.error(err));
+    };
+
+    const updateMessage = async (appointment) => {
+        await axios.post(`/ajax/messages/update/${id}`, {appointment: appointment}, {
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+            },
+        })
+            .then(res => addMessageAppointment(appointment))
+            .catch(err => console.error(err));
+    };
+
+    const addMessageAppointment = async (appointment) => {
+        await axios.post(`/ajax/messages/appointment/${room}`, {
+            appointment: appointment,
+            type: "answered-appointment",
+        }, {
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+            },
+        });
+    };
+
+    return (
+        <div id="appointment-card"
+             className="bg-white p-4 w-fit m-6">
+            <h4 className="font-bold text-xl">Rendez-vous {appointment.accepted ? "accepté" : "refusé"}</h4>
+            <br/>
+            {appointment.animals.map(animal => {
+                return (
+                    <div key={animal.id}
+                         className="flex justify-between">
+                        <p className="font-bold mr-2">Animal:</p>
+                        <p>{animal.fk_category.name} ({animal.race}) - {animal.name}</p>
+                    </div>
+                );
+            })}
+            <div className="flex justify-between">
+                <p className="font-bold mr-2">Date de rendez-vous:</p>
+                <p>{new Date(appointment.date[0]).toLocaleDateString()} - {new Date(appointment.date[1]).toLocaleDateString()}</p>
+            </div>
+            <div className="flex justify-between">
+                <p className="font-bold mr-2">Prix:</p>
+                <p>{appointment.price}€</p>
+            </div>
+            <br/>
+            {authorId != userId && !appointment.answered ? (
+                <div className="flex justify-end mt-3">
+                    <button onClick={() => answeredAppointment(appointment, false)}
+                            type="button"
+                            className="mr-2">Refuser
+                    </button>
+                    <button onClick={() => answeredAppointment(appointment, true)}
+                            type="button">Accepter
+                    </button>
+                </div>
+            ) : null}
+        </div>
+    );
+}
--- assets/react/controllers/chat/BubbleMessage.jsx
+++ assets/react/controllers/chat/BubbleMessage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {endpoint} from "../../../config";
-import axios from "axios";
+import Appointment from "./Appointment";
 
 export default function BubbleMessage({content, publicationDate, authorId, userId, shape, type, id, room}) {
     const [classNameP, setClassNameP] = useState("bg-blue-dark-purple");
@@ -8,7 +8,7 @@ export default function BubbleMessage({content, publicationDate, authorId, userI
     const [classNameShape, setClassNameShape] = useState("");
     const [imgAvailable, setImgAvailable] = useState(true);
     const [contentState, setContentState] = useState(content);
-    
+
     useEffect(() => {
         if (authorId == userId) {
             setClassNameP("bg-turquoise");
@@ -24,131 +24,41 @@ export default function BubbleMessage({content, publicationDate, authorId, userI
         }
     }, []);
 
-    const answeredAppointment = async (appointment, answer) => {
-        await axios.post("/ajax/appointments", appointment, {
-            headers: {
-                "X-Requested-With": "XMLHttpRequest",
-            }
-        })
-        .then(res => {
-            appointment.answered = true;
-            appointment.accepted = answer;
-            updateMessage(appointment).then(
-                setContentState(JSON.stringify(appointment))
-            );
-        })
-        .catch(err => console.error(err));
-    }
-
-    const updateMessage = async (appointment) => {
-        await axios.post(`/ajax/messages/update/${id}`, {appointment: appointment}, {
-            headers: {
-                "X-Requested-With": "XMLHttpRequest",
-            }
-        })
-        .then(res => addMessageAppointment(appointment))
-        .catch(err => console.error(err));
-    }
-    
-    const addMessageAppointment = async (appointment) => {
-        await axios.post(`/ajax/messages/appointment/${room}`, {
-            appointment: appointment, 
-            type: "answered-appointment"
-        }, {
-            headers: {
-                "X-Requested-With": "XMLHttpRequest"
-            }
-        });
-    }
-
     const bubbleContent = () => {
-        switch(type){
-            // Prise de RDV -> appointmentAnswered.accepted = true -> ACCEPTER ou appointmentAnswered.accepted = false -> REFUSER
-            case "answered-appointment":
-                const appointmentAnswered = JSON.parse(contentState);
-                return(
-                    <div id="appointment-card" className="bg-white p-4 w-fit m-6">
-                        <h4 className="font-bold text-xl">Rendez-vous {appointmentAnswered.accepted ? ("accepté") : "refusé"}</h4>
-                        <br />
-                        {appointmentAnswered.animals.map(animal => {
-                            return(
-                                <div key={animal.id} className="flex justify-between">
-                                    <p className="font-bold mr-2">Animal:</p>
-                                    <p>{animal.fk_category.name} ({animal.race}) - {animal.name}</p>
-                                </div>
-                            )
-                        })}
-                        <div className="flex justify-between">
-                            <p className="font-bold mr-2">Date de rendez-vous:</p>
-                            <p>{new Date(appointmentAnswered.date[0]).toLocaleDateString()} - {new Date(appointmentAnswered.date[1]).toLocaleDateString()}</p>
-                        </div>
-                        <div className="flex justify-between">
-                            <p className="font-bold mr-2">Prix:</p>
-                            <p>{appointmentAnswered.price}€</p>
-                        </div>
-                    </div>
-                );
-            // Prise de RDV en cours de réponse
-            case "appointment":
-                const appointment = JSON.parse(contentState);
-                return(
-                    <div id="appointment-card" className="bg-white p-4 w-fit m-6">
-                        <h4 className="font-bold text-xl">Demande de rendez-vous</h4>
-                        <br />
-                        {appointment.animals.map(animal => {
-                            return(
-                                <div key={animal.id} className="flex justify-between">
-                                    <p className="font-bold mr-2">Animal:</p>
-                                    <p>{animal.fk_category.name} ({animal.race}) - {animal.name}</p>
-                                </div>
-                            )
-                        })}
-                        <div className="flex justify-between">
-                            <p className="font-bold mr-2">Date de rendez-vous:</p>
-                            <p>{new Date(appointment.date[0]).toLocaleDateString()} - {new Date(appointment.date[1]).toLocaleDateString()}</p>
-                        </div>
-                        <div className="flex justify-between">
-                            <p className="font-bold mr-2">Prix:</p>
-                            <p>{appointment.price}€</p>
-                        </div>
-                        <br />
-                        {authorId != userId && !appointment.answered ? (
-                            <div className="flex justify-end mt-3">
-                                <button onClick={() => answeredAppointment(appointment, false)} type="button" className="mr-2">Refuser</button>
-                                <button onClick={() => answeredAppointment(appointment, true)} type="button">Accepter</button>
-                            </div>
-                        ) : null}
-                    </div>
-                );
-            // Message simple
-            case "message":
-            default:
-                return (
-                    <div id={`message-bubble-${authorId}`}
-                    className={`m-4 flex ${classNameDiv} px-24`}>
-                    {shape && authorId != userId ? (imgAvailable ? (
-                            <img src={`${endpoint.img}/users/user-${authorId}-1.jpg`}
-                                    className="w-[64px] h-[64px] self-end object-cover rounded-full"
-                                    onError={() => setImgAvailable(false)}
-                                    alt="image d'utilisateur"/>
-                        ) : (
-                            <div className="w-[64px] h-[64px] self-end rounded-full bg-gray-200"></div>
-                        )) : authorId != userId ? (
-                            <div className="w-[64px]"></div>
-                        ) : null}
-                        {shape ? (
-                                <p className={`${classNameP} relative max-w-[50%] rounded-[30px] w-fit ml-8 py-4 px-8 ${classNameShape}`}>{contentState}</p>)
-                            : (
-                                <p className={`${classNameP} relative max-w-[50%] rounded-[30px] w-fit ml-8 py-4 px-8`}>{contentState}</p>
-                            )}
-                        {authorId == userId ? (
-                            <div className="w-[64px]"></div>
-                        ) : null}
-                    </div>
-                );
-        }
-    }
-    
+        return (
+            <div id={`message-bubble-${authorId}`}
+                 className={`m-4 flex ${classNameDiv} px-24`}>
+                {shape && authorId != userId ? (imgAvailable ? (
+                    <img src={`${endpoint.img}/users/user-${authorId}-1.jpg`}
+                         className="w-[64px] h-[64px] self-end object-cover rounded-full"
+                         onError={() => setImgAvailable(false)}
+                         alt="image d'utilisateur"/>
+                ) : (
+                    <div className="w-[64px] h-[64px] self-end rounded-full bg-gray-200"></div>
+                )) : authorId != userId ? (
+                    <div className="w-[64px]"></div>
+                ) : null}
+                {shape ? (
+                        <p className={`${classNameP} relative max-w-[50%] rounded-[30px] w-fit ml-8 py-4 px-8 ${classNameShape}`}>
+                            {type === "appointment" && <Appointment contentState={contentState}
+                                                                    authorId={authorId}
+                                                                    userId={userId}
+                                                                    room={room}
+                                                                    id={id}/>}
+                            {type === "answered-appointment" &&
+                                <h1 className="text-3xl font-ChunkFive">Rendez-vous accepté !</h1>}
+                            {type === "message" && contentState}
+                        </p>)
+                    : (
+                        <p className={`${classNameP} relative max-w-[50%] rounded-[30px] w-fit ml-8 py-4 px-8`}>{contentState}</p>
+                    )}
+                {authorId == userId ? (
+                    <div className="w-[64px]"></div>
+                ) : null}
+            </div>
+        );
+    };
+
     return (
         bubbleContent()
     );
