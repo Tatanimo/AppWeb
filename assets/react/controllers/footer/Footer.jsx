a42722c4211ffd0ef7bb1be2e87918c2d6ff7e42
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-06-02 16:31:54 +0200

    feat: responsive footer feat: responsive services

--- assets/react/controllers/footer/Footer.jsx
+++ assets/react/controllers/footer/Footer.jsx
@@ -7,74 +7,90 @@ import {endpoint} from "../../../config";
 export default function Footer() {
     return (
         <>
-            <div className="w-1/2 flex items-start justify-center">
-                <a href="/"
-                   className="w-1/2">
-                    <img src={`${endpoint.img}/icons/logo_tatanimo.svg`}
-                         alt="logo"
-                         className="w-full"/>
-                </a>
-            </div>
-            <div className="w-1/2 flex">
-                <div className="w-1/2">
-                    <ul className="flex flex-col gap-6">
-                        <li href="#">
-                            <ThirdHeader content="Social"
-                                         color="white"
-                                         className=""/>
-                        </li>
-                        <a href="#"
-                           className="hover:underline text-white">
-                            <Paragraph content="Instagram"
-                                       color="white"
-                                       className=""/>
-                        </a>
-                        <a href="#"
-                           className="hover:underline text-white">
-                            <Paragraph content="Linkedin"
-                                       color="white"
-                                       className=""/>
-                        </a>
-                        <a href="#"
-                           className="hover:underline text-white">
-                            <Paragraph content="Facebook"
-                                       color="white"
-                                       className=""/>
-                        </a>
-                    </ul>
+            <div className="flex lg:flex-row flex-col w-full">
+                <div className="w-1/2 flex items-start justify-center">
+                    <a href="/"
+                       className="w-1/2">
+                        <img src={`${endpoint.img}/icons/logo_tatanimo.svg`}
+                             alt="logo"
+                             className="w-full"/>
+                    </a>
                 </div>
-                <div className="w-1/2">
-                    <ul className="flex flex-col gap-6">
-                        <li>
-                            <ThirdHeader content="Mentions"
-                                         color="white"
-                                         className=""/>
-                        </li>
-                        <a href="#"
-                           className="hover:underline text-white">
-                            <Paragraph content="Mentions légales"
-                                       color="white"
-                                       className=""/>
-                        </a>
-                        <a href="/about"
-                           className="hover:underline text-white">
-                            <Paragraph content="Qui sommes-nous ?"
-                                       color="white"
-                                       className=""/>
-                        </a>
-                        <a href="#"
-                           className="hover:underline text-white">
-                            <Paragraph content="Services"
-                                       color="white"
-                                       className=""/>
-                        </a>
-                        <a href="/contact"
-                           className="hover:underline text-white">
-                            <Paragraph content="Contact"
-                                       color="white"
-                                       className=""/>
-                        </a>
-                    </ul>
+                <div className="w-1/2 flex items-center justify-center flex-col lg:flex-row">
+                    <div className="w-1/2">
+                        <ul className="flex flex-col gap-6">
+                            <li>
+                                <ThirdHeader content="Social"
+                                             color="white"
+                                             className=""/>
+                            </li>
+                            <li>
+                                <a href="#"
+                                   className="hover:underline text-white">
+                                    <Paragraph content="Instagram"
+                                               color="white"
+                                               className=""/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                   className="hover:underline text-white">
+                                    <Paragraph content="Linkedin"
+                                               color="white"
+                                               className=""/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                   className="hover:underline text-white">
+                                    <Paragraph content="Facebook"
+                                               color="white"
+                                               className=""/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="w-1/2">
+                        <ul className="flex flex-col gap-6">
+                            <li>
+                                <ThirdHeader content="Mentions"
+                                             color="white"
+                                             className=""/>
+                            </li>
+                            <li>
+                                <a href="#"
+                                   className="hover:underline text-white">
+                                    <Paragraph content="Mentions légales"
+                                               color="white"
+                                               className=""/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/about"
+                                   className="hover:underline text-white">
+                                    <Paragraph content="Qui sommes-nous ?"
+                                               color="white"
+                                               className=""/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                   className="hover:underline text-white">
+                                    <Paragraph content="Services"
+                                               color="white"
+                                               className=""/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/contact"
+                                   className="hover:underline text-white">
+                                    <Paragraph content="Contact"
+                                               color="white"
+                                               className=""/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </>
