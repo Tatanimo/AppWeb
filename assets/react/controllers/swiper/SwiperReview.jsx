124d3655e870ea9da20dff7cecb1a59274f2ce5b
LucasBertaud <l.bertaud@hotmail.com>
2024-06-08 20:41:43 +0200

    style: responsive swiper

--- assets/react/controllers/swiper/SwiperReview.jsx
+++ assets/react/controllers/swiper/SwiperReview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReviewCard from "../review/ReviewCard";
 // Import Swiper React components
 import {Swiper, SwiperSlide} from "swiper/react";
@@ -8,51 +8,70 @@ import "swiper/css/navigation";
 import {Navigation} from "swiper/modules";
 
 export default () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+        setWidth(window.innerWidth);
+        };
+
+        // Ajouter un event listener pour la mise à jour de la taille de la fenêtre
+        window.addEventListener('resize', handleResize);
+
+        // Nettoyage du event listener lors du démontage du composant
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const getSlidesPerView = () => {
+        return width > 1440 ? 4.25 : width > 1024 ? 3.25 : width > 640 ? 2.25 : 1.25;
+    }
+
     return (
         <Swiper
-            slidesPerView={1}
-            spaceBetween={16}
-            navigation={true}
+            slidesPerView={getSlidesPerView()}
+            spaceBetween={32}
             loop={true}
-            modules={[Navigation]}
-            className="mySwiper flex gap-1 overflow-hidden [&_.swiper-slide]:!w-full xl:w-[1150px] lg:w-[700px] w-[300px]"
+            className="mySwiper flex gap-1 overflow-hidden"
         >
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={1}
                             name="Garde de Rockett"/>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={3}
                             name="Garde de Rockett"/>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={4}
                             name="Garde de Rockett"/>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={5}
                             name="Garde de Rockett"/>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={5}
                             name="Garde de Rockett"/>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={5}
                             name="Garde de Rockett"/>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={5}
                             name="Garde de Rockett"/>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="[&>div]:!flex-col">
                 <ReviewCard img="img/background/dog-with-toy.png"
                             intStar={5}
                             name="Garde de Rockett"/>
